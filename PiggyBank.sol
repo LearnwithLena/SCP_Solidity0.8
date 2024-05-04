14ad3ae7317e269d07f61c93bdb41b439301192f
LearnwithLena <102678151+LearnwithLena@users.noreply.github.com>
2024-05-04 11:10:26 +0100

    Piggy Bank App

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -2,20 +2,19 @@
 pragma solidity ^0.8.7;
 
 contract PiggyBank {
-
     // Events
     event Receive(uint amount);
     event Withdraw(uint amount);
 
     address public owner = msg.sender;
 
-    receive() external payable{
-        emit Receive(uint msg.value);
+    receive() external payable {
+        emit Receive(msg.value);
     }
 
-    function Withdraw() external {
+    function withdraw() external {
         require(msg.sender == owner, "Not Owner");
         emit Withdraw(address(this).balance);
-        selfdestruct(msg.sender);
+        selfdestruct(payable(msg.sender));
     }
 }
