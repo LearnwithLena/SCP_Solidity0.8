b40c6c3669946214232e4ffabb8e3323aca8ccb7
LearnwithLena <102678151+LearnwithLena@users.noreply.github.com>
2024-05-04 12:36:05 +0100

    Add natspec

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+/**
+ * @title Piggy Bank App
+ * @author SPC/ Lena
+ * @notice This contract is untested
+ * @dev A contract that receives ether from user, but should only withdraw to the owner of the contract
+ */
 contract PiggyBank {
     // Events //
     event Receive(uint amount);
@@ -12,6 +18,11 @@ contract PiggyBank {
         emit Receive(msg.value);
     }
 
+    /**
+     * @dev function that withdraws all the ether deposited into the contract
+     * @param -- no parameter given
+     * @return does not return any value
+     */
     function withdraw() external {
         require(msg.sender == owner, "Not Owner");
         emit Withdraw(address(this).balance);
